bff87306b3ec8f7c5b7db78faf42a26dbb03593a
Tsirimaholy <hei.tsirimaholy@gmail.com>
2022-04-04 13:39:12 +0300

    fix: Clean up code and fix some issue.

--- round2-level2/index.js
+++ round2-level2/index.js
@@ -1,21 +1,35 @@
-const dirReduc = (arrayDirection) => {
+export const dirReduc = (arrayDirection) => {
     const dirNum = {'NORD': 1, 'SUD': -1, 'EST': 2, 'OUEST': -2}
     const numDir = {"1": 'NORD', "-1": "SUD", "2": "EST", "-2": "OUEST"}
     arrayDirection = arrayDirection.map(dir => dirNum[dir])
 
-    arrayDirection = mapDirNum(arrayDirection);
     arrayDirection = mapDirNum(arrayDirection);
 
     return arrayDirection.map(e => numDir[e]);
 }
 
 function mapDirNum(arrayNum) {
+    let sum = 1;
     for (let i = 0; i < arrayNum.length; i++) {
+        /*
+        * reset the index to 0 and then redo all tasks
+        * because of the i++ of the for loop
+        * even if we reset it before "i++" will "i" increment by one
+        * */
+        if (sum === 0) i = 0;
+
         let prevDir = arrayNum[i];
         let nextDir = arrayNum[i + 1];
-        let sum = prevDir + nextDir;
-        if (sum === 0)
+        sum = prevDir + nextDir;
+
+        if (sum === 0) {
             arrayNum.splice(i, 2);
+            /*
+            * reset i to zero
+            * to prevent loop from breaking in case if (i+1)==(arrayNum.length)
+            * */
+            i = 0;
+        }
     }
     return arrayNum;
 }
--- round2-level3/index.js
+++ round2-level3/index.js
@@ -1,36 +1,24 @@
-const missingCArd = (cardList) => {
+export const missingCards = (cardList) => {
+    const possibleValue = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'V', 'D', 'R', 'A']
     const possibility = {
-        "C": [
-            '2', '3', '4',
-            '5', '6', '7', '8',
-            '9', '10', 'V'
-        ]
-        , "P": [
-            '2', '3', '4',
-            '5', '6', '7', '8',
-            '9', '10', 'V'
-        ]
-        , "T": [
-            '2', '3', '4',
-            '5', '6', '7', '8',
-            '9', '10', 'V'
-        ]
+        "C": [...possibleValue]
+        , "P": [...possibleValue]
+        , "Q": [...possibleValue]
+        , "T": [...possibleValue]
     };
+    // generate a regex to match cardList
     cardList = cardList.replaceAll(" ", "|");
     const regEx = new RegExp(cardList);
     let result = ""
 
     for (const posskey in possibility) {
+        // generate all 52 card list except for those in the cardList
         possibility[posskey].forEach(e => {
             let toPush = posskey + e;
-            /* if not in the list of cardList
-            * push it add it to the result list
-            */
-            if (!regEx.test(toPush)) result += toPush;
+            // skip card that was found in the cardList
+            if (!regEx.test(toPush)) result += toPush + " ";
         })
     }
 
-    return result;
+    return result.trimEnd();
 }
-
-console.table(missingCArd("C7 PV Q10"))
